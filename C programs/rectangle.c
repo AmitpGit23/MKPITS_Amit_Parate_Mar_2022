fd1ea767db9c8d1dd5dd2e1a00d7a17ed11be2a5
AmitpGit23 <amit.parate23@gmail.com>
2022-04-06 08:08:43 +0530

    rectangle

---
+++ C programs/rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <conio.h>
+
+void main() {
+  int height=7;
+  int width=5;
+  int area=width*height;
+  printf("Area of the rectangle : %d inch \n",area);
+  int peri=2*(height+width);
+  printf("perimeter of the rectangle : %d inch ",peri);
+  
+  }
