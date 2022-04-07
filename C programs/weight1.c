4c120461253723766a7820f5783d320f9808f184
AmitpGit23 <amit.parate23@gmail.com>
2022-04-07 07:38:13 +0530

    weight of two items

---
+++ C programs/weight1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float w1,w2,avg;
+	printf("Enter weight of Two items :");
+	scanf("%f %f",&w1,&w2);
+	avg=(w1+w2)/2;
+	printf("Average weight of Two item is : %f",avg);
+}
