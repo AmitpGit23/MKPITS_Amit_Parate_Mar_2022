d4672eb8b2c5123e29554891a8a6cb05da481f4f
AmitpGit23 <amit.parate23@gmail.com>
2022-04-09 08:24:02 +0530

    Wrong value or not

---
+++ C programs/que19_wrongvalue.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int p,q,r,s;
+	printf("Enter 1st integer =");
+	scanf("%d",&p);
+	printf("Enter 2nd integer =");
+	scanf("%d",&q);
+	printf("Enter 3rd integer =");
+	scanf("%d",&r);
+	printf("Enter 4th integer =");
+	scanf("%d",&s);
+	
+	if((r>0)&&(s>0)&&(p%2==0)&&(q>r)&&(s>p)&&((r+s)>(p+q)))
+	{
+		printf("Correct Value ! ");
+	}
+	else
+	{
+		printf("Wrong Value !");
+	}
+	
+}
