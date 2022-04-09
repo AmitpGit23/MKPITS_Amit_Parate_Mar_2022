e071e91103129dea006b0aff49155101f5b3c540
AmitpGit23 <amit.parate23@gmail.com>
2022-04-09 08:31:53 +0530

    integr form a Triangle if form find perimeter or invalid!

---
+++ C programs/Triangle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float a,b,c,peri;
+	printf("enter 1st side : ");
+	scanf("%f",&a);
+	printf("enter 2nd side : ");
+	scanf("%f",&b);
+	printf("enter 3rd side : ");
+	scanf("%f",&c);
+	
+	if(a<(b+c) && b<(a+c) && c<(a+b))
+	{
+		peri=a+b+c;
+		printf("\n perimeter = %f",peri);
+	
+	}
+	else
+	{
+		printf("Invalid values !");
+	}
+	
+	
+}
