34d353cadbc533f6da515f87ff0c924be309d5e9
AmitpGit23 <amit.parate23@gmail.com>
2022-04-09 08:26:17 +0530

    Sum of Odd value of 5 integer

---
+++ C programs/que22_sumoddvalue.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n1,n2,n3,n4,n5,add;
+	printf("input the first number : " );
+	scanf("%d",&n1);
+	printf("input the second number : " );
+	scanf("%d",&n2);
+	printf("input the third number : " );
+	scanf("%d",&n3);
+	printf("input the forth number : " );
+	scanf("%d",&n4);
+	printf("input the five number : " );
+	scanf("%d",&n5);
+	
+	if(n1%2==0){
+		
+	}
+	else{
+		add=add+n1;
+	}
+
+	if(n2%2==0){
+		
+	}
+	else{
+		add=add+n2;
+	}
+	
+	if(n3%2==0){
+
+	}
+	else{
+		add=add+n3;
+	}
+	
+	
+	if(n4%2==0){
+		
+	}
+	else{
+		add=add+n4;
+	}
+	
+	if(n5%2==0){
+		
+	}
+	else{
+		add=add+n5;
+	}
+	printf(" sum of Odd digit is %d",add);
+	
+	
+}
