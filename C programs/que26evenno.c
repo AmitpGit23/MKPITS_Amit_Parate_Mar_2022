8e83f121f7dda93afb3362c921e54adfcdbbd4a7
AmitpGit23 <amit.parate23@gmail.com>
2022-04-09 08:45:29 +0530

    Even No between 1 -50

---
+++ C programs/que26evenno.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i=0;
+	printf("Print even no between 0 to 50 : ");
+	while(i<=50)
+	{
+		if(i%2==0)
+		{
+			printf("%d ,",i);
+		}
+		i++;
+	}
+}
