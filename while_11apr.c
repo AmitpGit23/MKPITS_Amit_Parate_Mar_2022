6410e4a2c3d6f5233265dfc9bd33a4217417000a
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-04-18 16:47:18 +0530

    while loop

---
+++ while_11apr.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	//add 10 to 1 no ;
+	int i=10,add=0;
+	while(i>=1)
+	{
+		//printf("%d \n",i);
+		add=add+i;
+		printf("%d \n",add);
+		i--;
+		
+	}
+}
