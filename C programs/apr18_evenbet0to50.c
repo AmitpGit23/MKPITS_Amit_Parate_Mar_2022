ab1a498e82c693331328a6d77707d0d7a03bc1d3
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-04-18 16:52:22 +0530

    even no bet 0 to 50 using for loop

---
+++ C programs/apr18_evenbet0to50.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int i=0;
+	printf("Even No betweem  0 to 50 : ");
+	for(i=0;i<=50;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%d \n",i);
+		}
+	}
+}
