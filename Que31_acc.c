10cdc4fb5a7d36deccda09f54abd6d6c028ede78
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-04-19 07:30:06 +0530

    Accending order or not

---
+++ Que31_acc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a ,b,a1 ;
+	printf("please Enter 2 No : ");
+	scanf("%d %d",&a ,&b);
+	if(a !=b)
+	{
+		if(a<b)
+		{
+			printf("No is in accending order ");
+		}
+		else
+		{
+			printf("no is not in accending order ");
+		}
+				
+	}
+}
