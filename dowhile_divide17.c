c0681385f9e2342aff1e113b270369391c2333ec
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-04-19 12:20:06 +0530

    sum of no which is not divide by 17 using for loop

---
+++ dowhile_divide17.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n1,n2,t,i,sum=0;
+	printf("Enter 1st integer :");
+	scanf("%d",&n1);
+	printf("Enter 2nd integer :");
+	scanf("%d",&n2);
+	if(n1>n2)
+	{
+		t=n2;
+		n2=n1;
+		n1=t;
+	}
+	i=n1;
+	do
+	{
+		if((i%17)!=0)
+		{
+			sum=sum+i;
+		}
+		i++;
+	}
+	while(i<=n2);
+	printf("sum : %d",sum);
+}
