4c58ab32e068f96bf66f3125a5a6b9f8438d63e6
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-04-23 08:30:39 +0530

    print square cubes in a line

---
+++ que38_squcube.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,i,j=1,x=0;
+	printf("Input number of lines : ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d %d %d \n",i,i*i,i*i*i);
+	}
+}
