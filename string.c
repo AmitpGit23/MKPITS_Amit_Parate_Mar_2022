df506da9b48a209bf5e018a3851e7873a4c27bb5
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-04-27 11:42:53 +0530

    string class 1

---
+++ string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	//write to accept a string name from key and dis on screen
+	char name[10];
+//	char sname[20]={"Parate"};
+	printf("Enter your name : ");
+	scanf("%s",name);
+	printf("%s",name);
+	
+    
+    
+    
+}
