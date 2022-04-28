11070469fdf814346d14146b2f2eacc60370d5f4
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-04-28 14:45:57 +0530

    convert centimeter into inches

---
+++ que50_centitoinch.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+	float c,i;
+	printf("Enter a Distance in CM : ");
+	scanf("%f",&c);
+	
+	i=c/2.34;
+	printf("Distance of %f= %f inch",c,i);
+}
