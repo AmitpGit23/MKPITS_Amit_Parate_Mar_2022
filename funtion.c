8a9ccedf534317021a06c717b92727a86f70824f
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-04-29 10:35:28 +0530

    print name using function

---
+++ funtion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	disname();
+	disname();
+	disname();
+	
+}
+void disname()
+{
+	printf("Amit p\n");
+	getch();
+}
+//prototype : using  imformation to compile about function
