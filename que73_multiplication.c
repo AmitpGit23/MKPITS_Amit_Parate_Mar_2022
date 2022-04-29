c693922f17b14b5864ff291e77de5463f95da94a
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-04-29 10:39:40 +0530

    multiplication of three no

---
+++ que73_multiplication.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n1,n2,n3,mul;
+	printf("Enter first no : ");
+	scanf("%d",&n1);
+	printf("Enter second no : ");
+	scanf("%d",&n2);
+	printf("Enter third no : ");
+	scanf("%d",&n3);
+	
+	mul=n1*n2*n3;
+	
+	printf("Multiplication of 3 no = %d",mul);
+}
