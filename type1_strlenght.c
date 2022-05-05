f7ef6f9e5485edeb0a31704bd2d7557d8db6aea3
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-05-05 10:29:28 +0530

    string lenght using function type 1

---
+++ type1_strlenght.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	fun();
+	
+}
+int fun()
+{
+	char n1[10];
+	int i,c=0;
+	printf("Enter Any name : ");
+	scanf("%s",n1);
+	
+	for(i=0;n1[i]!='\0';i++)
+	{
+		c=c+1;
+	}
+	printf("Lenght=%d",c);
+}
