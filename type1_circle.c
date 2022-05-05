8c4ff3edd7084dec77ff3f08449972f08febd348
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-05-05 10:21:08 +0530

    area of circle using type1

---
+++ type1_circle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	crcl();
+}
+void crcl()
+{
+	int r=6;
+	float ar,peri;
+	peri=2*3.14*r;	
+	printf("perimeter = %.2f \n",peri);
+	ar=3.14*r,r;
+	printf("Araa= %.2f",ar);
+}
