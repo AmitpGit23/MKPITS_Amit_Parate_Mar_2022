77dcec8fdeb6774a53ccaac237b7470ae245d05f
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-05-15 20:13:54 +0530

    question no 91 return 30

---
+++ que91_sumRa10-20.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n1,n2,sum;
+	printf("Enter 1st No :  ");
+	scanf("%d",&n1);
+	printf("Enter 2nd No :  ");
+	scanf("%d",&n2);
+	sum=n1+n2;
+	if(sum>=10&& sum<=20)
+	{
+		printf("30");
+	}
+	else
+	{
+		return 0;
+	}
+	
+	
+}
