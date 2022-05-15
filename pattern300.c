dbdd4a2d47631d09ceb8097f8a9356dc659cf576
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2022-05-15 23:24:23 +0530

    pattern 300

---
+++ pattern300.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+#include<conio.h>  
+void main()  
+{  
+    int r,c,k,a=1;  
+
+    for(r=3;r>=1;r--)  
+    {  
+        for(c=1;c<=r-1;c++)  
+        {  
+          printf(" ");  
+        }  
+        for(k=1;k<=a;k++)  
+        {  
+            printf("%d",k);  
+        }  
+        printf("\n");  
+        a++;  
+    }  
+} 
